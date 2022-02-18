f78da3af48cd9a9ba94924b119d0ddb2629a1595
John Moshakis <john@moshakis.com>
2022-02-17 20:06:12 -0500

    Add extensions

---
+++ Storage/Extensions/RLMObjectExtensions.pas
@@ -0,0 +1,26 @@
+﻿namespace App.Core;
+
+uses
+  Realm;
+
+type
+
+  RLMObjectExtensions = public extension class(RLMObject)
+  private
+  protected
+  public
+
+    class method All<T>:sequence of T; where T is unconstrained;
+    begin
+      exit allObjectsInRealm(RLMRealm.defaultRealm).Cast<T>;
+    end;
+
+    class method All:RLMResults;
+    begin
+      exit allObjectsInRealm(RLMRealm.defaultRealm);
+
+    end;
+
+  end;
+
+end.
